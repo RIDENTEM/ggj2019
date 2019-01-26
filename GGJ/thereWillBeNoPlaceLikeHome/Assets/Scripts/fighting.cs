bdfbc4e273d0101a2e0b74ff069262478293d3ac
Jonathan Ribarro <Jcribarro@gmail.com>
2019-01-26 00:56:48 -0500

    Made a script for health and the fighting, untested as of now

---
+++ GGJ/thereWillBeNoPlaceLikeHome/Assets/Scripts/fighting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighting : MonoBehaviour
+{
+
+
+    [SerializeField] BoxCollider2D punchCollider;
+    Animator fightingController;
+    float punchForceStraight = 10.0f;
+    float punchForceUp = 5.0f;
+    bool onPunch1 = true;
+    bool onPunch2 = false;
+    bool onPunch3 = false;
+    bool[] punchNumber = { false, false, false };
+
+    void Start()
+    {
+        punchCollider = GetComponent<BoxCollider2D>();
+
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            punchCollider.enabled = true;
+            // fightingController.play
+        }
+        else
+            punchCollider.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject objectCollidedWith = collision.gameObject;
+        Rigidbody2D objectRigidbody = objectCollidedWith.GetComponent<Rigidbody2D>();
+        if (objectCollidedWith.tag == "Enemy")
+        {
+            if (onPunch1)
+                objectRigidbody.AddForce(new Vector2(punchForceStraight * 0.5f, 0.0f));
+            else if (onPunch2)
+                objectRigidbody.AddForce(new Vector2(punchForceStraight * 2.0f, punchForceUp * 0.5f));
+            else if (onPunch3)
+                objectRigidbody.AddForce(new Vector2(0.0f, punchForceUp * 2.0f));
+        }
+    }
+}
---
+++ GGJ/thereWillBeNoPlaceLikeHome/Assets/Scripts/health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour {
+
+    private int maxHealth = 3;
+    public static int currentHealth;
+    Collider2D homeBodyCollider;
+    Rigidbody2D homeRigidBody;
+    [SerializeField] SpriteRenderer[] healthBar;
+    
+
+	void Start () {
+        homeRigidBody = GetComponent<Rigidbody2D>();
+        for (int i = 0; i < healthBar.Length; i++)
+            healthBar[i].color = Color.green;
+        currentHealth = maxHealth;
+        homeBodyCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    if(collision.gameObject.tag == "Enemy")
+        {
+            currentHealth -= 1;
+            healthBar[currentHealth].color = Color.red;
+            homeRigidBody.AddForce(new Vector2(Vector2.left.x, 1.0f));
+        }
+    }
+
+    void Update () {
+		
+	}
+}
