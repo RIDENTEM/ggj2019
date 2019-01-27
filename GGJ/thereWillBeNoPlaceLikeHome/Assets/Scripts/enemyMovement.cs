96f280c275716fd531e7b527975cdec654089705
Jonathan Ribarro <Jcribarro@gmail.com>
2019-01-26 20:48:41 -0500

    Worked on navmesh agent for the enemy, works well for now. Will need to work on getting sounds in and the background as well

--- GGJ/thereWillBeNoPlaceLikeHome/Assets/Scripts/enemyMovement.cs
+++ GGJ/thereWillBeNoPlaceLikeHome/Assets/Scripts/enemyMovement.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour
 {
 
-    Vector3 patrolPos1;
-    Vector3 patrolPos2;
+
     Transform[] childPatrolPointTransforms;
+    List<Vector3> patrolPoints = new List<Vector3>();
+    int destPoint = 0;
+    NavMeshAgent enemyNavAgent;
+    bool firstPatrolPoint = true;
 
     void Start()
     {
+        //There are four points, it starts at the top and includes the gameobject this starts from, as in the parent
         childPatrolPointTransforms = GetComponentsInChildren<Transform>();
+        patrolPoints.Add(childPatrolPointTransforms[2].position);
+        patrolPoints.Add(childPatrolPointTransforms[3].position);
+        enemyNavAgent = GetComponentInChildren<NavMeshAgent>();
+       // enemyNavAgent.SetDestination(patrolPos1);
+
     }
 
-    // Update is called once per frame
+    void goToNextPoint()
+    {
+        enemyNavAgent.destination = patrolPoints[destPoint];
+        destPoint = (destPoint + 1) % patrolPoints.Count;
+    }
+
     void Update()
     {
 
+        if(!enemyNavAgent.pathPending && enemyNavAgent.remainingDistance < 0.95f)
+        {
+
+            goToNextPoint();
+            
+        }
     }
 }
