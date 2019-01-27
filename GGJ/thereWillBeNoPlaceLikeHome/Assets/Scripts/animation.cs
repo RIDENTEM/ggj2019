203beba6d8d4dfed76f0020b3892d27e0111f5aa
Austin <aph924@gmail.com>
2019-01-26 20:59:59 -0500

    Added animation

---
+++ GGJ/thereWillBeNoPlaceLikeHome/Assets/Scripts/animation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour {
+
+    Rigidbody2D rigid;
+    public GameObject legL;
+    public GameObject legR;
+    float timer = 0;
+    int dir = 1;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rigid.velocity.x < 0) dir = -1;
+        else if (rigid.velocity.x > 0) dir = 1;
+        else timer = 0;
+        updateLegs();
+	}
+
+    void updateLegs()
+    {
+        print(timer);
+        legL.transform.localScale = new Vector3(dir, 1, 1);
+        legR.transform.localScale = new Vector3(dir, 1, 1);
+        legL.transform.localEulerAngles = new Vector3(0, 0, Mathf.Sin(timer)*75);
+        legR.transform.localEulerAngles = new Vector3(0, 0, -Mathf.Sin(timer) * 75);
+        timer += Time.deltaTime * 10;
+    }
+}
