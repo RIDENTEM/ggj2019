57c1ace5532cc38874aac1fc597e3d693df881b9
Jonathan Ribarro <Jcribarro@gmail.com>
2019-01-26 19:52:30 -0500

    Working with the navmesh agent, apparently 2d navmeshes aren't possible, but by working in 3d it can be faked

---
+++ GGJ/thereWillBeNoPlaceLikeHome/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+
+    Vector3 patrolPos1;
+    Vector3 patrolPos2;
+    Transform[] childPatrolPointTransforms;
+
+    void Start()
+    {
+        childPatrolPointTransforms = GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
